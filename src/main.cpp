c72ddf1ce4b9da6b9a4e2bb2d508cca12924f982
Nandan V <nvpotti.mec@gmail.com>
2022-01-15 17:52:18 +0530

    CTRE deps and a simple example with std::regex.

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,13 @@
-#include <unordered_map>
+#include <regex>
 
 #include <fmt/format.h>
-#include <catch2/catch_all.hpp>
+//#include <catch2/catch_all.hpp>
 
-static constexpr int insertion_count = 100;
-static constexpr int index_to_access = insertion_count - 1;
-
-template<typename T>
-void
-insert(T &container, const int count) {
-	for (int i = 0; i < count; i++) {
-		container.insert(std::make_pair(i, 42));
-	}
-}
-
-std::unordered_map<int, int> unordered_mapInt;
-std::map<int, int> mapInt;
-
-TEST_CASE("For unordered map")
+int main()
 {
-	unordered_mapInt.clear();
+	std::regex re{"abc*|[0-9]+"};
 
-	SECTION("Inserting elements")
-	{
-		static constexpr int iInserted = 2;
-		insert(unordered_mapInt, iInserted);
-		REQUIRE(iInserted == unordered_mapInt.size());
-	}
+	fmt::print("{}",std::regex_match("123",re));
 
-	BENCHMARK(fmt::format("Insert {} elements", insertion_count))
-				{
-					return insert(unordered_mapInt, insertion_count);
-				};
-
-	BENCHMARK(fmt::format("Access {}th element", index_to_access))
-				{
-					return unordered_mapInt[index_to_access];
-				};
-}
-
-TEST_CASE("For map")
-{
-	mapInt.clear();
-
-	SECTION("Inserting elements")
-	{
-		static constexpr int iInserted = 2;
-		insert(mapInt, iInserted);
-		REQUIRE(iInserted == mapInt.size());
-	}
-
-	BENCHMARK(fmt::format("Insert {} elements", insertion_count))
-				{
-					return insert(mapInt, insertion_count);
-				};
-
-	BENCHMARK(fmt::format("Access {}th element", index_to_access))
-				{
-					return mapInt[index_to_access];
-				};
+	return 0;
 }
