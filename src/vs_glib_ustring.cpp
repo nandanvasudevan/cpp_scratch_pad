0327f964ffb9d06cb7cb8136906fc425eb1acfd0
nandanv <nvpotti.mec@gmail.com>
2022-07-03 16:16:10 +0530

    A hand-rolled custom formatter implementation is the best choice!

--- src/vs_glib_ustring.cpp
+++ src/vs_glib_ustring.cpp
@@ -1,7 +1,9 @@
 //
 // Created by nandanv on 17/6/22.
 //
+#include <algorithm>
 #include <cmath>
+#include <cstring>
 #include <iomanip>
 #include <string>
 
@@ -12,6 +14,8 @@
 #include <fmt/format.h>
 
 static const std::string sTag = "vs_Glib::ustring";
+
+static constexpr size_t BUFFER_SIZE = 20;
 
 Glib::ustring Glib_FormatDouble(const double dValue, const int iDecimals) {
     return Glib::ustring::format(std::setprecision(iDecimals),
@@ -26,7 +30,7 @@ Glib::ustring fmt_FormatDouble(const double dValue, const int iDecimals) {
 }
 
 Glib::ustring snprintf_FormatDouble(const double dValue, const int iDecimals) {
-    char sBuffer[20];
+    char sBuffer[BUFFER_SIZE];
 
     {
         char sFormat[8];
@@ -42,6 +46,28 @@ Glib::ustring snprintf_FormatDouble(const double dValue, const int iDecimals) {
     }
 
     return Glib::ustring(sBuffer);
+}
+
+Glib::ustring customFormatter(const double dValue, const size_t uiDecimals) {
+    size_t uiValue = static_cast<size_t>(dValue * pow(10,
+                                                      static_cast<double>(uiDecimals)));
+    char caValue[BUFFER_SIZE] = {};
+    size_t i = 0;
+
+    while (uiValue) {
+        caValue[i++] = '0' + static_cast<char>(uiValue % 10);
+
+        if (i == uiDecimals) {
+            caValue[i++] = '.';
+        }
+
+        uiValue /= 10;
+    }
+
+    std::reverse(caValue,
+                 caValue + i);
+
+    return Glib::ustring(caValue);
 }
 
 std::pair<int, int> BreakDouble(const double dValue, const int iDecimals) {
@@ -71,7 +97,7 @@ Glib::ustring fmt_FormatDoubleArithmetic(const double dValue, const int iDecimal
 }
 
 Glib::ustring snprintf_FormatDoubleArithmetic(const double dValue, const int iDecimals) {
-    char sBuffer[50] = {};
+    char sBuffer[BUFFER_SIZE] = {};
 
     auto [iWhole, iDecimal] = BreakDouble(dValue,
                                           iDecimals);
@@ -102,7 +128,7 @@ TEST_CASE("Simple format",
 
     BENCHMARK("snprintf")
                 {
-                    char sBuffer[10];
+                    static char sBuffer[10];
                     snprintf(sBuffer,
                              sizeof(sBuffer),
                              "%d",
@@ -112,7 +138,7 @@ TEST_CASE("Simple format",
 
     BENCHMARK("snprintf to Glib::ustring")
                 {
-                    char sBuffer[10];
+                    static char sBuffer[10];
                     snprintf(sBuffer,
                              sizeof(sBuffer),
                              "%d",
@@ -162,26 +188,33 @@ TEST_CASE("Float format",
 TEST_CASE("Custom decimals",
           sTag /*+ "[.]"*/)
 {
+    static constexpr size_t uiDecimals = 9;
+
     SECTION("Test")
     {
         REQUIRE_FALSE(Glib::ustring("3.141592653") == Glib_FormatDouble(M_PI,
-                                                                        9));
+                                                                        uiDecimals));
         REQUIRE(Glib::ustring("3.141592654") == Glib_FormatDouble(M_PI,
-                                                                  9));
+                                                                  uiDecimals));
         REQUIRE_FALSE(Glib::ustring("3.141592653") == fmt_FormatDouble(M_PI,
-                                                                       9));
+                                                                       uiDecimals));
         REQUIRE(Glib::ustring("3.141592654") == fmt_FormatDouble(M_PI,
-                                                                 9));
+                                                                 uiDecimals));
 
         REQUIRE(Glib::ustring("3.141592653") == Glib_FormatDoubleArithmetic(M_PI,
-                                                                            9));
+                                                                            uiDecimals));
         REQUIRE(Glib::ustring("3.141592653") == fmt_FormatDoubleArithmetic(M_PI,
-                                                                           9));
+                                                                           uiDecimals));
 
         REQUIRE_FALSE(Glib::ustring("3.141592653") == snprintf_FormatDouble(M_PI,
-                                                                            9));
+                                                                            uiDecimals));
         REQUIRE(Glib::ustring("3.141592654") == snprintf_FormatDouble(M_PI,
-                                                                      9));
+                                                                      uiDecimals));
+
+        REQUIRE_FALSE(Glib::ustring("3.141592654") == customFormatter(M_PI,
+                                                                      uiDecimals));
+        REQUIRE(Glib::ustring("3.141592653") == customFormatter(M_PI,
+                                                                uiDecimals));
     }
 
     static constexpr int iMaxDecimals = 15;
@@ -206,7 +239,11 @@ TEST_CASE("Custom decimals",
                                 return Glib_FormatDouble(M_PI,
                                                          iDecimals);
                             };
-
+                BENCHMARK("Custom formatter")
+                            {
+                                return customFormatter(M_PI,
+                                                       iDecimals);
+                            };
             }
         }
     }
